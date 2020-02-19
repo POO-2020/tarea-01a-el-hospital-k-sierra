b9b17c87879a3cd9e625f03d8c1739e67fc3809b
Karla Sierra <ksierra@ucol.mx>
2020-02-18 18:02:00 -0600

    Fecha terminada

--- Fecha.js
+++ Fecha.js
@@ -1,28 +1,31 @@
 class Fecha{
-    constructor(dia, mes, año)
+    constructor(dia, mes, año, años, meses, semanas, dias)
     {
         this.fecha = new Date (año, mes + 1, dia);
         this.diaSemana = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
-        this.mes = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
         this.fechaAct = new Date;
-        var totalMeses = 12;
     }
-
     getAños(){
         let años = this.fechaAct.getUTCFullYear() - this.fecha.getFullYear();
-        console.log(`Han pasado ${años} años`);
+        console.log(`${años} años`);
     }
     getMeses(){
-        let meses = this.fechaAct.getUTCMonth() - this.fecha.getFullMonth();
-        console.log(`Han pasado ${meses} meses`);
+        let año = this.fechaAct.getUTCFullYear() - this.fecha.getFullYear();
+        let meses = año * 12;
+        console.log(`${meses} meses`);
     }
     getSemanas(){
-        var anos = this.fechaAct.getUTCFullYear() - this.fecha.getFullYear();
-        var mesesTotales = anos* Number.parseInt(totalMeses);
-        console.log("Han pasado " + mesesTotales + " meses");
+        let año = this.fechaAct.getUTCFullYear() - this.fecha.getFullYear();
+        let meses = año * 12;
+        let semanas = meses * 4;
+        console.log(`${semanas} semanas`);
     }
     getDias(){
-
+        let año = this.fechaAct.getUTCFullYear() - this.fecha.getFullYear();
+        let meses = año * 12;
+        let semanas = meses * 4;
+        let dias = semanas * 7;
+        console.log(`${dias} semanas`);
     }
     getFecha(){
         console.log(`${this.fecha.getDate()}/${this.fecha.getMonth()}/${this.fecha.getFullYear()}`);
@@ -32,9 +35,3 @@ class Fecha{
         console.log(`${dia}`);
     }
 }
-
-let fecha1 = new Fecha(7,2,2000);
-fecha1.getAños();
-fecha1.getMeses();
-fecha1.getFecha();
-fecha1.getDiaFecha();
