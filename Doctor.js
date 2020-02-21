346914281325c3b4ac9274989706ab5ee6d00878
Karla Sierra <ksierra@ucol.mx>
2020-02-21 16:57:47 -0600

    Doctor avance

---
+++ Doctor.js
@@ -0,0 +1,19 @@
+import Nombre from "./Nombre.js";
+class Doctor{
+    /**
+     * 
+     * @param {number} cedula 
+     * @param {string} especialidad 
+     * @param {Nombre} nombre 
+     * @param {number} telefono 
+     */
+    constructor(cedula, especialidad, nombre, telefono){
+        this.cedula = cedula;
+        this.especialidad = especialidad;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    getPerfil(){
+        return `${this.cedula}, ${this.especialidad}, ${this.nombre.getNombreCompleto()}`;
+    }
+}
